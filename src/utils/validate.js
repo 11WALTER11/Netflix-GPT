72100184f665d969289be75664079bfee94e3341
Mohammad Arish Siddiqui <81313264+11WALTER11@users.noreply.github.com>
2024-02-17 01:16:15 +0530

    Update validate.js

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,7 +1,6 @@
 export const validateForm = (email, password) => {
-  const isEmailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
-  const isPasswordValid =
-    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+  const isEmailValid = /^[\w.-]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+  const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(password);
 
   if (!isEmailValid) return "Please enter a valid email";
   if (!isPasswordValid) return "Please enter a valid password";
