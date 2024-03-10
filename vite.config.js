a6a977cafe564442c36126018ed62089bc2693ec
Mohammad Arish Siddiqui <81313264+11WALTER11@users.noreply.github.com>
2024-03-10 19:47:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src",
+export default ({ command, mode }) => {
+  // Check if the build command is in production mode
+  const isProduction = mode === "production";
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
     },
-  },
-  server: {
-    port: 3002,
-  },
-  base: "/Netflix-GPT/",
-});
+    server: {
+      port: 3002,
+    },
+    base: isProduction ? "/Netflix-GPT/" : "/",
+  });
+};
