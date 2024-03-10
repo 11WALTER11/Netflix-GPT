d37137c05f315f6b944c9e349d176b2048ac6e07
Mohammad Arish Siddiqui <81313264+11WALTER11@users.noreply.github.com>
2024-03-10 19:32:27 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,21 +4,23 @@ export const USER_AVATAR =
   "https://occ-0-4409-3646.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABcdk1iCbqnp0L9eChv7Q8IHGZ2WTy26xTot4zHhnhnjjBiVicIkUNo9qBqvdD49rOnefHYhJ_ghofRGnfHobQ87SzOh_J4E.png?r=a4f";
 export const BANNER_IMAGE =
   "https://assets.nflxext.com/ffe/siteui/vlv3/5e16108c-fd30-46de-9bb8-0b4e1bbbc509/29d8d7d7-83cc-4b5f-aa9b-6fd4f68bfaa6/IN-en-20240205-popsignuptwoweeks-perspective_alpha_website_small.jpg";
+
+// Use Vite environment variables for sensitive information
 export const TMDB_API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    // Authorization: import.meta.env.VITE_TMDB_API_KEY,
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNGQyMjU3YTQ1MDU1NjRmOWRmMDFiZTlmM2YwNjcyOCIsInN1YiI6IjYxNzc4Zjg2ZDIzNmU2MDA5MDFhNDgwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.24jk_EVa80szuiZ43QfAcrDcX50Cbdkp8U2BRU3Xufw",
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
   },
 };
+
 export const TMDB_IMAGE_CDN = "https://image.tmdb.org/t/p/w500/";
+
 export const SUPPORTED_LANGUAGES = [
   { identifier: "en", name: "English" },
   { identifier: "hi", name: "Hindi" },
   { identifier: "ur", name: "Urdu" },
 ];
-// export const OPEN_API_KEY = import.meta.env.VITE_OPEN_API_KEY;
-export const OPEN_API_KEY =
-  "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";
+
+// Use Vite environment variable for API key
+export const OPEN_API_KEY = import.meta.env.VITE_OPEN_API_KEY;
