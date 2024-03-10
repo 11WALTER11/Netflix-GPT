7aad04665df5e88d2dd79e3d62b1f5c9f79d29e9
Mohammad Arish Siddiqui <81313264+11WALTER11@users.noreply.github.com>
2024-03-10 20:57:31 +0530

    Update firebase.js

--- src/http/firebase.js
+++ src/http/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional.
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || process.env.VITE_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || process.env.FIREBASE_API_KEY,
   authDomain: "netflix-gpt-70623.firebaseapp.com",
   projectId: "netflix-gpt-70623",
   storageBucket: "netflix-gpt-70623.appspot.com",
   messagingSenderId: "591100923561",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || process.env.VITE_FIREBASE_APP_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || process.env.FIREBASE_APP_ID,
   measurementId: "G-WT35CGG8TR",
 };
 
